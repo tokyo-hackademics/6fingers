0ec19da3d8a9d65989c45bf2fdfeab0ad22731b1
PhanVanTrung <vantrungphan2012@gmail.com>
2015-05-16 19:52:56 +0900

    Test2 bo dung

---
+++ Elarm/app/src/main/java/com/fingers/six/elarm/AlarmFragment.java
@@ -0,0 +1,40 @@
+package com.fingers.six.elarm;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by PhanVanTrung on 2015/05/16.
+ */
+public class AlarmFragment extends Fragment {
+
+    public AlarmFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_alarm, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
